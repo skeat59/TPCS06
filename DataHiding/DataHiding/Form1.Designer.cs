ff93854dbeabeab4449d20e6a027aa6688094f2d
skeat59 <gdalia_t@epita.fr>
2014-01-24 17:15:38 +0100

    blabla

---
+++ DataHiding/DataHiding/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace DataHiding
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.load = new System.Windows.Forms.Button();
+            this.hideButton = new System.Windows.Forms.Button();
+            this.reveal = new System.Windows.Forms.Button();
+            this.opendialog = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // load
+            // 
+            this.load.Location = new System.Drawing.Point(28, 83);
+            this.load.Name = "load";
+            this.load.Size = new System.Drawing.Size(75, 23);
+            this.load.TabIndex = 0;
+            this.load.Text = "load";
+            this.load.UseVisualStyleBackColor = true;
+            this.load.Click += new System.EventHandler(this.load_Click);
+            // 
+            // hideButton
+            // 
+            this.hideButton.Location = new System.Drawing.Point(251, 83);
+            this.hideButton.Name = "hideButton";
+            this.hideButton.Size = new System.Drawing.Size(75, 23);
+            this.hideButton.TabIndex = 1;
+            this.hideButton.Text = "hideButton";
+            this.hideButton.UseVisualStyleBackColor = true;
+            this.hideButton.Click += new System.EventHandler(this.hideButton_Click);
+            // 
+            // reveal
+            // 
+            this.reveal.Location = new System.Drawing.Point(508, 83);
+            this.reveal.Name = "reveal";
+            this.reveal.Size = new System.Drawing.Size(75, 23);
+            this.reveal.TabIndex = 2;
+            this.reveal.Text = "revealButton";
+            this.reveal.UseVisualStyleBackColor = true;
+            this.reveal.Click += new System.EventHandler(this.reveal_Click);
+            // 
+            // opendialog
+            // 
+            this.opendialog.FileName = "opendialog";
+            this.opendialog.FileOk += new System.ComponentModel.CancelEventHandler(this.opendialog_FileOk);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(28, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(555, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "message a inserer :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 184);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.reveal);
+            this.Controls.Add(this.hideButton);
+            this.Controls.Add(this.load);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button load;
+        private System.Windows.Forms.Button hideButton;
+        private System.Windows.Forms.Button reveal;
+        private System.Windows.Forms.OpenFileDialog opendialog;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DataHiding/DataHiding/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DataHiding
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Bitmap image;
+        private void load_Click(object sender, EventArgs e)
+        {
+            opendialog.ShowDialog();
+            image = new Bitmap(opendialog.FileName);
+        }
+
+        private void hideButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void reveal_Click(object sender, EventArgs e)
+        {
+            if (image != null)
+            {
+                int NbPx = image.Height * image.Width;
+                FileStream fs = new FileStream(image, FileMode.open, FileAccess.Read);
+                BinaryReader br = new BinaryReader(fs);
+
+            }
+        }
+
+        private void opendialog_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
